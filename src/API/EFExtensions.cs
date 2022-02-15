bd0c340ececf9935c022d8e44a88e4f84f837c60
Victor Marante <Victor.Marante@sigma.se>
2022-02-15 15:56:48 +0100

    Seed product table

---
+++ src/API/EFExtensions.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API;
+
+public static class EFExtensions
+{
+    public static void Clear<T>(this DbSet<T> set) where T : class => set.RemoveRange(set);
+}
