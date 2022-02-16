40d9e3dc23012df55dc3a960883458951e10d5e2
Victor Marante <Victor.Marante@sigma.se>
2022-02-16 15:47:00 +0100

    Add tests for db context transactions

--- src/Infrastructure.EF/AppDbContext.cs
+++ src/Infrastructure.EF/AppDbContext.cs
@@ -11,7 +11,8 @@ namespace Infrastructure.EF
     {
         private IDbContextTransaction _currentTransaction;
 
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
         {
         }
 
